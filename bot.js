76a4b3fc14d5abc9ef9433e074ad812efe4a08c1
aswinprasad <aswinprasad999@gmail.com>
2021-05-06 15:04:56 +0530

    withcredentials:true

--- bot.js
+++ bot.js
@@ -166,7 +166,9 @@ axios.get(aliasFunc(x),
    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
     'cache-control':'no-cache',
     'scheme':'https'
- }
+ },
+ withCredentials: true
+
 
 })
   .then(response => {
