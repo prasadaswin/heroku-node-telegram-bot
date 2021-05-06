b8bb766f98eb6d368a83a6f28dcddfa61c3a6568
aswinprasad <aswinprasad999@gmail.com>
2021-05-06 15:08:12 +0530

    add  'X-Requested-With': 'XMLHttpRequest' to axios

--- bot.js
+++ bot.js
@@ -165,7 +165,8 @@ axios.get(aliasFunc(x),
  headers:{
    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
     'cache-control':'no-cache',
-    'scheme':'https'
+    'scheme':'https',
+     'X-Requested-With': 'XMLHttpRequest'
  },
  withCredentials: true
 
